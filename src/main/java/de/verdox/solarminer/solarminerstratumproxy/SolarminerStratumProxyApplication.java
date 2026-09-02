85940e5215f481273149f8876d7cf877c8228373
Verdox <mail.ysp@web.de>
2026-09-02 13:43:25 +0200

    enable scheduling

--- src/main/java/de/verdox/solarminer/solarminerstratumproxy/SolarminerStratumProxyApplication.java
+++ src/main/java/de/verdox/solarminer/solarminerstratumproxy/SolarminerStratumProxyApplication.java
@@ -2,8 +2,10 @@ package de.verdox.solarminer.solarminerstratumproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SolarminerStratumProxyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SolarminerStratumProxyApplication.class, args);
